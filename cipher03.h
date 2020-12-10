e6c6f9af241eef72174d2c2cd826d5bb229e897b
Brenton Trebilcock <tre15004@byui.edu>
2020-12-10 13:00:32 -0500

    Finished psuedocode for the Affine Cipher, also added another source for the citation.

--- cipher03.h
+++ cipher03.h
@@ -26,9 +26,18 @@ public:
     ***********************************************************/
    virtual std::string getCipherCitation()
    {
-      std::string str = "“Implementation of Affine Cipher.”";
+      std::string str = "“Implementation of Affine Cipher.” ";  //source 1
        str += "GeeksforGeeks, 29 May 2019, ";
-       str += "www.geeksforgeeks.org/implementation-affine-cipher/. \n";
+       str += "www.geeksforgeeks.org/implementation-affine-cipher/. \n\n";
+
+       str += "Borodzhieva, A. N., & Manoilov, P. K. (2014)."; //source 2
+       str += "Training module with graphical user interface for encryption \n";
+       str += "and decryption using affine ciphers applied in cryptosystems. ";
+       str +=  "2014 IEEE 20th International Symposium for \n";
+       str += "Design and Technology in Electronic Packaging (SIITME), ";
+       str += "Design and Technology in Electronic Packaging\n";
+       str += "(SIITME), 2014 IEEE 20th International Symposium For, 281–286.\n";
+       str += "https://doi.org/10.1109/SIITME.2014.6967045\n";
       return str;
    }
 
@@ -41,19 +50,44 @@ public:
       std::string str;
       // The encrypt pseudocode
       str = "Encryption pseudocode: \n";
-      str += "encrypt(plaintext, password) \n";
-      str += ""; //NEED TO figure out what to use password for?
-      str += "";
-      str += "";
-      str += "RETURN cipherText";
+      str += "encrypt(plainText, password) \n"; //the password needs to contain 2 keys separated by a space
+      str += "key1 <- password.substring(0,3)\n"; 
+      str += "key2 <- password.substring(3)\n";
+      str += "IF key1 or key2 are not numbers\n";
+      str += " RETURN error\n";   
+      str += "IF key1 is not a coprime of alphabetSize\n";
+      str += " RETURN error\n";
+      str += "IF key2 is greater than alphabetSize or key2 is less than zero\n";
+      str += " RETURN error\n";
+      str += "FOR v in alphabetSize\n";
+      str += " alphabetArray[v] <- character of the alphabet at value\n";
+      str += "FOR v in length of plainText\n";
+      str += " plainTextArray[v] <- the corresponding integer value for the character\n";
+      str += "FOR v in size of plainTextArray\n";
+      str += " cipherTextArray <- (key1*plainTextArray[v]+key2) % alphabetSize\n";
+      str += "FOR v in size of cipherTextArray\n";
+      str += " cipherText += alphabetArray[cipherTextArray[v]]";           
+      str += "RETURN cipherText\n\n";
       // The decrypt pseudocode
       str += "Decryption pseudocode: \n";
-      str += "";
-      str += "";
-      str += "";
-      str += "";
-      str += "";
-      str += "RETURN plainText";
+      str += "decrypt(cipherText, password)\n";
+      str += "key1 <- password.substring(0,3)\n"; 
+      str += "key2 <- password.substring(3)\n";
+      str += "IF key1 or key2 are not numbers\n";
+      str += " RETURN error\n";   
+      str += "IF key1 is not a coprime of alphabetSize\n";
+      str += " RETURN error\n";
+      str += "IF key2 is greater than alphabetSize or key2 is less than zero\n";
+      str += " RETURN error\n";
+      str += "FOR v in alphabetSize\n";
+      str += " alphabetArray[v] <- character of the alphabet at value\n";
+      str += "FOR v in length of cipherText\n";
+      str += " cipherTextArray[v] <- the corresponding integer value for the character\n";
+      str += "FOR v in size of cipherTextArray\n";
+      str += " plainTextArray <- key1^-1 * (cipherTextArray[v]-key2) % alphabetSize\n";    
+      str += "FOR v in size of plainTextArray\n";
+      str += " plainText += alphabetArray[plainTextArray[v]]";         
+      str += "RETURN plainText\n";
 
       return str;
    }
